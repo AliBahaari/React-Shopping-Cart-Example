500b36f7cde72c5236f00227a3c122f4cbfbdb00
Ali Bahaari <alibahaari1997@gmail.com>
2021-09-14 01:30:16 +0430

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+
+  const cartBox = useRef(null);
+
+  const [data, setData] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+  const [CartItemsVisible, setCartItemsVisible] = useState('none');
+
+  useEffect(() => {
+
+    const dataUrl = axios({
+      url: 'https://jsonplaceholder.typicode.com/posts',
+      method: 'get'
+    });
+
+    dataUrl.then(res => {
+      setData(res.data);
+    });
+
+  }, []);
+
+  const clickCartButton = (productId, productTitle, productBody) => {
+
+    setCartItems([
+      ...cartItems,
+      {
+        id: productId,
+        title: productTitle,
+        url: productBody
+      }
+    ]);
+
+  }
+
+  const showCartItems = (e) => {
+    setCartItemsVisible('block');
+  }
+
+  const hideCartItems = (e) => {
+
+    if (!cartBox.current.contains(e.target)) {
+      setCartItemsVisible('none');
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" onClick={ hideCartItems }>
+
+      <div className="cartIcon" onClick={ showCartItems } ref={ cartBox }>
+        Cart
+        {
+          cartItems.length > 0 ?
+          <div className="badge">{ cartItems.length }</div>
+          :
+          <div className="badge">0</div>
+        }
+
+        {
+          cartItems.length > 0 ?
+            <div className="cartItems" style={{ display: CartItemsVisible }}>
+              {
+                cartItems.map(cartItem => (
+                  <div className="cartItemTitle">{ cartItem.title }</div>
+                ))
+              }
+              <button className="checkoutButton">Check Out</button>
+            </div>
+            :
+            <div className="cartItems" style={{ display: CartItemsVisible }}>
+              <div className="cartError">× No Items!</div>
+            </div>
+        }
+      </div>
+
+      <div className="products">
+        {
+          data.map(product => (
+            <div className="productBlock">
+              <div className="productPlaceholder"></div>
+              <p className="productTitle">{ product.title }</p>
+              <button className="cartButton" onClick={ () => clickCartButton(product.id, product.title, product.body) }>Add to Cart</button>
+            </div>
+          ))
+        }
+      </div>
+
     </div>
   );
 }
